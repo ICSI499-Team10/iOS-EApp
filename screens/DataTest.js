af74d3ea6627d7c5d1e847330c4b969c34796134
evnpoon <evnpoon@gmail.com>
2022-03-23 12:59:00 -0400

    Progress that kind of works

--- screens/DataTest.js
+++ screens/DataTest.js
@@ -2,6 +2,57 @@ import React, { useEffect, useState } from 'react';
 import {StyleSheet, View, Text, Button, ActivityIndicator, FlatList} from 'react-native';
 
 const mainfunction = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        try {
+            /*
+            const response = await fetch('https://reactnative.dev/movies.json');
+            const json = await response.json();
+            console.log(json);
+            setData(json.movies);
+            */
+
+            const response = await fetch('http://eapp-test.arcc.albany.edu/publish/Incident', {
+                method: 'GET',
+                headers: {
+                    "AuthToken": "br22&@uJbpFyH2UayJwG*rfvFKjye7m!",
+                    "RecentDate": null
+                }
+            });
+            const json = await response.json();
+            console.log(json);
+            setData(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+                <Text>{item.title}, {item.timeOfEvent}</Text>
+                //<Text>{item.title}, {item.releaseYear}</Text>
+            )}
+            />
+            )}
+        </View>
+    );
+};
+
+/*
+const mainfunction = () => {
     return fetch('http://eapp-test.arcc.albany.edu/publish/Incident', {
         method: 'GET',
         headers: {
@@ -19,45 +70,6 @@ const mainfunction = () => {
     });
 };
 
-/*
-const mainfunction = () => {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-
-    const getData = async () => {
-        try {
-            const response = await fetch('http://eapp-test.arcc.albany.edu/publish/Incident', {
-                method: 'GET',
-                headers: {
-                    AuthToken: 'br22&@uJbpFyH2UayJwG*rfvFKjye7m!',
-                    RecentDate: 'null'
-                }
-            })
-            const jsondata = await response.json();
-            setData(jsonData);
-            console.log(jsondata);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-
-        useEffect(() => {
-            getData();
-        }, []);
-
-        return (
-            <View style={styles.screen}>
-                {isLoading ? <ActivityIndiciator /> : (
-                    <FlatList data={data} />
-                )}
-                <Text>Test Text</Text>
-            </View>
-        )
-    }
-}
-*/
-
 const styles = StyleSheet.create({
   screen: { 
     flex: 1, 
@@ -65,5 +77,6 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   }
 })
+*/
 
 export {mainfunction}
