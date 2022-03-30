1bfddd050bd4e45a1d758dffcd080e524bdca41a
Christos Markakis <s22christos@test.com>
2022-03-30 12:53:10 -0400

    imported icons, going to start messing around with them

--- navigators/bottomTabNav.js
+++ navigators/bottomTabNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {AdvisoryScreen} from '../screens/Advisory'; 
