a64091c9f7e09243a91cd51067074ff542515d35
Christos Markakis <s22christos@test.com>
2022-04-04 11:52:00 -0400

    I made a global style sheet that wont even work on anything LMAO formatting hard :(

---
+++ styles/globalStyles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native-web";
+
+export const globalStyles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 24,
+        color: 'red',
+    }
+});
+
