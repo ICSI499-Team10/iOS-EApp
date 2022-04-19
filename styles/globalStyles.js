6381567f82c3db67e69422cf5fbeb42d4128e8a7
Christos Markakis <s22christos@test.com>
2022-04-19 11:43:52 -0400

    okay we got some good progress here, alligned the correct way

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -3,7 +3,6 @@ import {StyleSheet} from "react-native";
 export const globalStyles = StyleSheet.create({
     stackNavStyle:{
         height:100,
-        justifyContent: "top",
         alignItems: "flex-start",
         flex: 1,
         flexDirection: "row"
