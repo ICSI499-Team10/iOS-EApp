f86952d7bc5eec8020eb51a647a57b968e4955f5
evnpoon <evnpoon@gmail.com>
2022-04-26 17:35:38 -0400

    start of update user info

--- navigators/stackAccNav.js
+++ navigators/stackAccNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack'
+import {createStackNavigator} from '@react-navigation/stack';
 import {AccountScreen} from '../screens/Account';
+import {ProfileScreen} from '../screens/AccountScreens/Profile';
 import {SettingsScreen} from '../screens/AccountScreens/Settings'; 
 import {ContactScreen} from '../screens/AccountScreens/Contact'; 
 import {AboutScreen} from '../screens/AccountScreens/About';
@@ -12,6 +13,7 @@ const StackAccNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name="Account" component={AccountScreen} />
+            <Stack.Screen name="My Profile" component={ProfileScreen}/>
             <Stack.Screen name="Settings" component={SettingsScreen} />
             <Stack.Screen name="Contact" component={ContactScreen} />
             <Stack.Screen name="About" component={AboutScreen} />
---
+++ screens/AccountScreens/Profile.js
@@ -0,0 +1,80 @@
+import React from 'react'; 
+import { Text, View, StyleSheet, Button} from 'react-native';
+
+const ProfileScreen = props => { 
+    console.log(props);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mac, setMAC] = useState('');
+    const [password, setPassword] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [emergencyName, setEmergencyName] = useState('');
+    const [emergencyPhone, setEmergencyPhone] = useState('');
+
+    const req = {
+        method: "POST",
+        headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        "userType": "Visitor",
+        "user": {
+            "name": name,
+            "email": email,
+            "mac": '3c:06:30:38:b6:14',
+            "password": password,
+            "address": address,
+            "phone": phone,
+            "emergencyName": emergencyName,
+            "emergencyPhone": emergencyPhone
+        }
+        })
+    };
+
+    const submitHandler = async () => {
+        try {
+            const URI = "http://eapp-test.arcc.albany.edu/publish/UpdateAccount";
+            const response = await fetch(URI, req);
+            const dataJSON = await response.json();
+            console.log(dataJSON); 
+        } catch(error) {
+            console.log(error);
+        };
+        if(reponse.status == 200) {
+            alert("Successfully updated information!");
+        } else {
+            alert("Error: try again later.");
+        }
+    };
+
+    return (
+        <View style={styles.screen}>
+            <Text>Profile Screen</Text>
+            <Text>Update User Information</Text>
+            <TextInput placeholder={name} onChangeText={name => setName(name)}/>
+            <TextInput placeholder='Email' onChangeText={email => setEmail(email)}/>
+            <TextInput placeholder='Phone Number' onChangeText={phone => setPhone(phone)}/>
+            <TextInput placeholder='Password' secureTextEntry = {true} onChangeText={password => setPassword(password)}/>
+            <TextInput placeholder='Address' onChangeText={address => setAddress(address)}/>
+            <TextInput placeholder='Emergency Contact' onChangeText={emergencyName => setEmergencyName(emergencyName)}/>
+            <TextInput placeholder='Emergency Contact Phone Number' onChangeText={emergencyPhone => setEmergencyPhone(emergencyPhone)}/>
+            <Button 
+                title = "Save Changes"
+                onPress = {() => submitHandler(this)}
+            />
+            <Button title="Go Back" onPress={() => props.navigation.goBack()}/>
+        </View>   
+    )
+}
+
+const styles = StyleSheet.create({
+  screen: { 
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export {ProfileScreen}
