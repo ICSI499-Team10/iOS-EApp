f46ebd6bdaedc109b0e0d8e68d6cb4ca71f0f396
Christos Markakis <s22christos@test.com>
2022-04-25 21:41:37 -0400

    figured out something?

--- screens/AccountScreens/Settings/Settings.js
+++ screens/AccountScreens/Settings/Settings.js
@@ -1,11 +1,38 @@
 import React from 'react'; 
-import {Text, View, StyleSheet, Button, SectionList, StatusBar, SafeAreaView} from 'react-native'; 
+import {Text, View, StyleSheet, FlatList, Button, StatusBar, SafeAreaView} from 'react-native'; 
+
+const SettingsData = [
+  {
+    title: 'Item 1',
+  },
+  {
+    title: 'Item 2',
+  },
+  {
+    title: 'Item 3',
+  },
+
+];
+
+const Item = ({title}) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
+
 
 const SettingsScreen = props => { 
+
+  const renderItem = ({item}) => (
+    <Item title={item.title}/>
+  );
+
   return (
       <SafeAreaView style={styles.screen}>
-        
-
+        <FlatList
+          data={SettingsData}
+          renderItem={renderItem}
+        />
       </SafeAreaView>  
   )
 }
