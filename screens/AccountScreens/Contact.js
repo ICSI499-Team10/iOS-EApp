27ade7e6b1b23b1ebaf301017b1619e206c30593
evnpoon <evnpoon@gmail.com>
2022-05-10 15:14:39 -0400

    more styling

--- screens/AccountScreens/Contact.js
+++ screens/AccountScreens/Contact.js
@@ -1,15 +1,19 @@
 import React from 'react'; 
 import { Text, View, StyleSheet, Button, Linking } from 'react-native';
+import {globalStyles} from '../../styles/globalStyles';
 
 const ContactScreen = props => { 
     return (
-        <View style={styles.screen}>
-            <Text>Contact Us Screen</Text>
-            <Text>Found an issue with the application? Send a report to our developers here:</Text>
-            <Button
-              title = "EApp Issue Report"
-              onPress = {() => Linking.openURL("https://gitreports.com/issue/UbiNet-Research/eapp")}
-            />
+        <View style={globalStyles.loginContainer}>
+            <Text style={globalStyles.titleText}>Contact Us</Text>
+            <Text style={globalStyles.titleText2}>Found an issue with the application? Send a report to our developers here:</Text>
+            <View style={globalStyles.registerButtons}>
+              <Button
+                title = "EApp Issue Report"
+                onPress = {() => Linking.openURL("https://gitreports.com/issue/UbiNet-Research/eapp")}
+                color = "white"
+              />
+            </View>
         </View>   
     )
 }
